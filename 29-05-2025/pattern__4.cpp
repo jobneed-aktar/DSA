8a3f8169758e75d890dc85b4f749d7f47e063309
Aktaruzzaman <jobneed.aktar@gmail.com>
2025-05-29 12:07:47 +0600

    inverted triangle pattern with charecter version

--- 29-05-2025/pattern__4.cpp
+++ 29-05-2025/pattern__4.cpp
@@ -20,6 +20,22 @@ int main() {
         cout << endl;
     }
 
+        /*
+            Inverted triangle pattern charecter version
+        */ 
+        int n = 4;
+        char ch = 'A';
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+            cout << " "; 
+            }
+            for(int j = 0; j < n - i; j++) {
+            cout << ch;
+            }
+            ch++;
+            cout << endl;
+        }
+
 
 
 
