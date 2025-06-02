84e2634a287d3e3aad122f8f7063f90b3b9d966e
Aktaruzzaman <jobneed.aktar@gmail.com>
2025-06-02 23:02:16 +0600

    Two pointer function

--- dsa.cpp
+++ dsa.cpp
@@ -1,22 +1,29 @@
-#include <bits/stdc++.h>
-// #include<iostream>
+#include <iostream>
+#include <climits>
 using namespace std;
 
+
+void reverseArr(int arr[], int size) {
+    int start = 0; 
+    int end = size -1;
+    while(start < end) {
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
+
+
 int main() {
+    int arr[] = {10,20,30,40,50};
+    int size = sizeof(arr) / sizeof(int);
+    reverseArr(arr,size);
+
+    for(int i = 0; i < size -1 ; i++) {
+       cout << arr[i] << endl;
+    }
     
     
-    
-   
-    
-
-    
-
-
-
-
-
-
-
 
 
     return 0;
